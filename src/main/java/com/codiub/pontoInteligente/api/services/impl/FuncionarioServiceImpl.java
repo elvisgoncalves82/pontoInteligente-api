6758bd9f5808114bcdfdf0a061e2f013a7bac70a
elvis <elvisrgonclaves@hotmail.com>
2020-03-31 08:16:08 -0300

    impl service

---
+++ src/main/java/com/codiub/pontoInteligente/api/services/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,43 @@
+package com.codiub.pontoInteligente.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.codiub.pontoInteligente.api.entities.Funcionario;
+import com.codiub.pontoInteligente.api.repositories.FuncionarioRepository;
+import com.codiub.pontoInteligente.api.services.FuncionarioService;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+	
+	private static final Logger log = LoggerFactory.getLogger(FuncionarioServiceImpl.class);
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+
+	@Override
+	public Funcionario persistir(Funcionario funcionario) {
+		log.info("Persistindo funcionário: {}", funcionario);
+		return this.funcionarioRepository.save(funcionario);
+	}
+
+	@Override
+	public Optional<Funcionario> buscarPorCpf(String cpf) {
+		log.info("Buscando funcionários pelo CPf {}" ,cpf);
+		return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
+	}
+
+	@Override
+	public Optional<Funcionario> buscarPorEmail(String email) {
+		log.info("Buscando funcionario pelo email {}", email);
+		return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
+	}
+
+	
+
+	
+}
